d688d9717db658b5ec3bd42353c14ba242414cf8
Fabricio Zuardi <fabricio@fabricio.org>
2012-08-22 12:02:56 -0300

    relógio funcionando

--- js/main.js
+++ js/main.js
@@ -15,6 +15,25 @@ function tabClicked(event){
     tab.addClass('active');
   }
 }
+function updateCounter(){
+  if (!miliseconds_left){
+    var final_date = new Date(2012, 8, 19 , 0, 0, 0, 0).getTime(); //19 de setembro de 2012
+    var current_date = Date.now();
+    var miliseconds_left = final_date - current_date;
+  }
+  var days_left =  Math.max(0, Math.floor(miliseconds_left / 86400000));
+  var hours_left = Math.max(0, Math.floor((miliseconds_left % 86400000)/1000/60/60));
+  var digits = [
+    Math.floor(days_left / 10),
+    days_left % 10,
+    Math.floor(hours_left / 10),
+    hours_left % 10
+  ];
+  $('#contador .digit .number').each(function(i){
+    $(this).html(digits[i]);
+  });
+}
 $(document).ready(function() {
   $('header nav a').click(tabClicked);
+  updateCounter();
 });
--- todo.md
+++ todo.md
@@ -4,7 +4,9 @@
 - OK - remover o pontilhadinho default do browser do link ativo nas tabs
 - OK - numeros com opacidade 80%
 - OK - cada balão pode ter largura e posicionamento diferente
+- OK - relogio funcionando em javascript
 
+- adaptacao pro php prover quantos milisegundos faltam
 - para onde devem os links de facebook e twitter linkar?
 - botar o username do cara do lado da foto, como em http://blog.instagram.com/post/24408162773/whp-watchingwatchers
 - fazer balao fechar tambem quando vc clica fora dele
