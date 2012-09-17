f257df2db47a8cd198969ce4c04a71bc574ce78a
Fabricio Zuardi <fabricio@fabricio.org>
2012-09-17 00:41:17 -0300

    callback para assinar tags real-time

--- instagram_callback.php
+++ instagram_callback.php
@@ -1,24 +1,27 @@
-
 <?php
+if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+  print $_GET['hub_challenge'];
+  // var_dump($_GET);
+} else {
 
+$instagram_update = file_get_contents('php://input');
+
+
+//generate log
 ob_start();
-
-var_dump($_REQUEST);
-var_dump(file_get_contents('php://input'));
-
+var_dump($instagram_update);
 $output = ob_get_contents();
-
 ob_end_clean();
 
-echo $output;
-
 $file = 'instagram_log.txt';
-// Open the file to get existing content
 $current = file_get_contents($file);
-// Append a new person to the file
 $current .= "\n=====================\n";
 $current .= $output;
 $current .= "\n---------------------\n";
-// Write the contents back to the file
 file_put_contents($file, $current);
-?>
+
+
+
+//update db
+
+} ?>
