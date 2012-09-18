06c8162d85b0bfb50010c7da81e4a45efc424773
Fabricio Zuardi <fabricio@fabricio.org>
2012-09-17 23:42:40 -0300

    bug de um checkbox ativar o outro

--- index-promo-admin.php
+++ index-promo-admin.php
@@ -82,13 +82,13 @@ foreach ($result as $entry) {
           <fieldset>
             <p class="username-label">' . $entry["username"] . '</p>
             <label class="photo-id"><textarea>' . $entry["photo_id"] . '</textarea>
-            <label>
+            <span>
               <i class="destaque-icon"></i>
               <i class="blacklist-photo-icon"></i>
               <input type="checkbox" name="blacklisted-photos[' . $entry["photo_id"] . ']" />
               <i class="blacklist-user-icon"></i>
               <input type="checkbox" name="blacklisted-users[' . $entry["username"] . ']" />
-            </label>
+            </span>
           </fieldset>
         </li>';
 }
